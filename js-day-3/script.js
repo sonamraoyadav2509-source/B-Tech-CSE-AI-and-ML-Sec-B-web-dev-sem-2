b47bf16b12370a63d2cee36d5b2e27f720281dc2
Sonam Yadav <sonamraoyadav2509@gmail.com>
2026-01-22 15:24:55 +0530

    New

--- js-day-3/script.js
+++ js-day-3/script.js
@@ -1,13 +1,45 @@
-let sentence ="I am a comma seperated sentence"
-let words = sentence.split(" ");
-console.log(words)
+// let sentence ="I am a comma seperated sentence"
+// let words = sentence.split(" ");
+// console.log(words)
 
-let sentence2 = "I am a comma separated sentence"
-let words2= sentence2.split("a");
-console.log(words2)
+// let sentence2 = "I am a comma separated sentence"
+// let words2= sentence2.split("a");
+// console.log(words2)
 
-//Q1. find out the occurancee of "r" in the given string.
+// //Q1. find out the occurancee of "r" in the given string.
 
-let str = "tu meri me tera me teri tu mera";
-let word = str.split(" r ")
-console.log(word)
+// let str = "tu meri me tera me teri tu mera";
+// let word = str.split(" r ")
+// console.log(word)
+
+function demo(){
+      console.log("demo")
+}
+demo()
+function sample(a,b){
+    return a+b;
+}
+console.log(samples(5,10))
+
+//arrow function 
+//advantage : doesn't need return and function keyword and it will perform asynchronously
+ const sample=(c,d)=>{
+    return c+d;
+ }
+ console.log(test(4,6))
+
+let arr = [1,2,3,4,5]
+ let res = arr.map(function(x){
+         return x+2
+ })
+ console.log(res)
+ setTimeout(callback,timedelay);
+
+ setTimeout(()=>{
+    console.log("sign up")
+ },1000)
+
+ let date = new Date();
+console.log(date)
+
+
---
+++ js-day-4/script.js
@@ -0,0 +1,9 @@
+let user = {
+    FullName:"Sonam Yadav",
+    address:"Gurugram",
+    mobileno:2341567892,
+    favcolor:["red","blue"]
+}
+//console.log(user.FullName,user.mobileno,user.favcolor[1]);
+console.log(user["FullName"]);
+
