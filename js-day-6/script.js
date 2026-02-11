3c5394c30abde84c714e7898d9aa48f7eba89d23
Sonam Yadav <sonamraoyadav2509@gmail.com>
2026-02-11 14:39:22 +0530

    update

---
+++ js-day-6/script.js
@@ -0,0 +1,7 @@
+const headingElement = document.
+getElementById('myheading');
+console.log(headingElement)
+
+headingElement.textContent='new heading';
+headingElement.style.color='blue';
+headingElement.style.backgroundColor='yellow';
---
+++ js-day-6/scripttt.js
@@ -0,0 +1,8 @@
+// Select the first <p> element inside the #content div using querySelector
+const paragraph = document.querySelector("#content p");
+
+// Change the text content of the selected paragraph
+paragraph.textContent = "This paragraph has been updated using querySelector.";
+
+// Change the style of the selected paragraph
+paragraph.style.color = "blue";
